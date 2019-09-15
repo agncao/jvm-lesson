30341ff5e2d65906e355d453574f91853446c91e
caojianmin <caojianmin@jd.com>
2019-09-15 18:52:12 +0800

    jvm

--- src/main/java/com/caojm/lessons/jvm/methodstack/MethodStackTestUnit.java
+++ src/main/java/com/caojm/lessons/jvm/methodstack/MethodStackTestUnit.java
@@ -1,7 +1,7 @@
 package com.caojm.lessons.jvm.methodstack;
 
 /**
- * ${DESCRIPTION}
+ * 一个方法开辟一块栈桢内存区域
  *
  * @author <a href=mailto:caojianmin@jd.com>caojianmin1</a>
  * @create 2019-09-08 下午5:15
@@ -15,12 +15,12 @@ public class MethodStackTestUnit {
     public static void main(String[] args) {
         MethodStackTestUnit testUnit = new MethodStackTestUnit();
         System.out.println(testUnit.increaseStep(3));
-        Thread t = new Thread(new Runnable() {
-            public void run() {
-                System.out.println("start to run");
-            }
-        });
-        t.start();
+//        Thread t = new Thread(new Runnable() {
+//            public void run() {
+//                System.out.println("start to run");
+//            }
+//        });
+//        t.start();
 
     }
 
