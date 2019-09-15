f907c00e30fccbbc4a08ba4859c3e6153507720d
caojianmin <caojianmin@jd.com>
2019-09-15 23:14:31 +0800

    blank

--- src/main/java/com/caojm/lessons/jvm/methodstack/MethodStackTestUnit.java
+++ src/main/java/com/caojm/lessons/jvm/methodstack/MethodStackTestUnit.java
@@ -1,7 +1,10 @@
 package com.caojm.lessons.jvm.methodstack;
 
 /**
+ * 当创建一个线程时，同时会创建一个栈，栈的大小和深度都是固定的
  * 一个方法开辟一块栈桢内存区域
+ * iconst_1 将常量 1 压到栈定
+ *
  *
  * @author <a href=mailto:caojianmin@jd.com>caojianmin1</a>
  * @create 2019-09-08 下午5:15
